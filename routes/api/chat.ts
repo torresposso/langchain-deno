acdb6c0ff578192beea390edc9747c583e3dbe18
torresposso <erictorres.co@gmail.com>
2023-06-04 22:45:32 -0500

    renoive chat

--- routes/api/chat.ts
+++ routes/api/chat.ts
@@ -20,29 +20,6 @@ export async function handler(req: Request) {
 
   const { userInput } = await req.json();
 
-  const { readable, writable } = new TransformStream();
-  const writer = writable.getWriter();
-  const reader = readable.getReader();
-  const writerX = writerFromStreamWriter(writer);
-
-  console.log("user", userInput);
-  const response = await chain.call(
-    { input: userInput },
-    [
-      {
-        handleLLMNewToken: async (token) => {
-          const text = new TextEncoder().encode(`data: ${token}`);
-          const xr = await writeAll(writerX, text);
-          console.log("called", xr);
-        },
-      },
-    ],
-  );
-
-  console.log("res", response);
-
-  const streamedToken = await reader.read();
-
   return new Response(streamedToken?.value, {
     headers: {
       ...corsHeaders,
